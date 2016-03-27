7cb0002dab2f9a37eb62cf6b4aa542fecbbd36b5
coyotemix <coyote_mixplayas@hotmail.com>
2016-03-27 15:08:48 -0500

    funcion isnan

--- scripts.js
+++ scripts.js
@@ -1,46 +1,8 @@
-console.log("Inicio programa");
-var numeroUsuario = prompt("Ingrese un numero del 1 al 5: ");
-var numeroAdivinar = 3;
-if (parseInt(numeroUsuario) ===numeroAdivinar)
+function numeroAlAzarHastaLimite(limite)
 {
-adivinoCorrectamenete = true;
+	 var num=Math.floor(Math.random()*limite)+1;
+	 return num;
+}
 
-}
-else  if (parseInt(numeroUsuario) > numeroAdivinar)
-{
-	alert("el numero que ingreso es mayor ");
-	var nuevoIntento = prompt("intenta de nuevo un numero del 1 al 5:");
-	if (parseInt(nuevoIntento) === numeroAdivinar)
-
-{
-  adivinoCorrectamenete = true;
-}
-else
-{ 
-	adivinoCorrectamenete = false;
-
-}
-}
-else if (parseInt(numeroUsuario)<numeroAdivinar)
-{
-alert("el numero que ingresastes es menor");
-var nuevoIntento = prompt("intente de nuevo, ingrese numero del 1 al 5:");
-if (parseInt(nuevoIntento) === numeroAdivinar)
-
-{
-	adivinoCorrectamenete= true;
-}
-else
-{
-     adivinoCorrectamenete= false;
- }
-}
-if (adivinoCorrectamenete)
-{
-	document.write("<p>adivinastes</p>");
-}
-else
-{
-	document.write("<p>no adivinastes</p>");
-}
-console.log("programa completo");
+var numAzar = numeroAlAzarHastaLimite(50);
+alert("el numero al azar es: "+numAzar);
