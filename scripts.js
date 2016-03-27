42bcd467678f37f3514f3a9f5f82e4ec64b90add
coyotemix <coyote_mixplayas@hotmail.com>
2016-03-27 10:07:43 -0500

    arreglar repositorio

--- scripts.js
+++ scripts.js
@@ -1,12 +1,15 @@
 console.log("Inicio de programa");
-<<<<<<< HEAD
-alert("Hola JavaScript");
-document.write("<h1>Hola JS UPSE</h1>");
+var preguntas *3;
+var preguntasFaltantes *"Quedan" +""preguntas+"preguntas";
+alert ("este es el juego del cuenta cuentos. sigue las instrucciones.");
+var sustantivo= prompt("ingrese un sustantivo." +preguntasFaltantes);
+preguntas=preguntas-1;
+preguntasFaltantes="quedan"+ preguntas+"preguntas";
+var adjetivo = prompt("estima usuario;ingrese un adjetivo."+preguntasFaltantes);
 
-alert("Hola Mundo");
-document.write("<h1>Hola amigos, esto es JavaScript!</h1>");
-
-alert("Gracias por visitar");
+document.write("<h1>cuanta cuaentos</h1>");
+document.write("<p>"+suatantivo+" "+adjetivo+" "+verbo+"locamente</p>");
+document.write(<p>por cuando "+verbo+" el "+sustantivo+" siempre es "+adjetivo+",</p>);
+document.write("<p> por eso el "+suatantivo+" siene que es muy "+adjetivo+" y es asi que cada dia "+verbo+".</P>);
 console.log("Fin de programa");
-
 
