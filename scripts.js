b8fdb7a0636bf4e51190fbdba89f8fc3abf6de5e
coyotemix <coyote_mixplayas@hotmail.com>
2016-03-27 15:35:25 -0500

    desafio 1

--- scripts.js
+++ scripts.js
@@ -1,8 +1,17 @@
-function numeroAlAzarHastaLimite(limite)
+function numAlAzar(lim1,lim2)
 {
-	 var num=Math.floor(Math.random()*limite)+1;
-	 return num;
+	var num= Math.floor(Math.random()*(lim1-lim2))+ lim2;
+		return num;
 }
-
-var numAzar = numeroAlAzarHastaLimite(50);
-alert("el numero al azar es: "+numAzar);
+var lim1 = parseInt(prompt("ingrese limite superior: "));
+if (isNaN(lim1))
+{
+	throw Error("no es el numero valido");
+}
+var lim2 = parseInt(prompt("ingrese limite inferior:"));
+if (isNaN(lim2))
+{
+	throw Error("no es el numero valido");
+}
+var numero = numAlAzar(lim1,lim2);
+alert("el numero al azar es: "+numero);
