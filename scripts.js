eccc545e92b553711e1e3290e6804d5dfe935b4c
coyotemix <coyote_mixplayas@hotmail.com>
2016-03-27 12:26:42 -0500

    ejercicio de funciones tira dados

--- scripts.js
+++ scripts.js
@@ -1,13 +1,46 @@
 console.log("Inicio programa");
 var numeroUsuario = prompt("Ingrese un numero del 1 al 5: ");
 var numeroAdivinar = 3;
-if (numeroUsuario ===numeroAdivinar)
+if (parseInt(numeroUsuario) ===numeroAdivinar)
 {
-	alert("Adivinastes. good Job.!!!");
-}
-else 
-{
-	alert("No adivinastes, vuelva intentar =(");
-}
+adivinoCorrectamenete = true;
 
+}
+else  if (parseInt(numeroUsuario) > numeroAdivinar)
+{
+	alert("el numero que ingreso es mayor ");
+	var nuevoIntento = prompt("intenta de nuevo un numero del 1 al 5:");
+	if (parseInt(nuevoIntento) === numeroAdivinar)
+
+{
+  adivinoCorrectamenete = true;
+}
+else
+{ 
+	adivinoCorrectamenete = false;
+
+}
+}
+else if (parseInt(numeroUsuario)<numeroAdivinar)
+{
+alert("el numero que ingresastes es menor");
+var nuevoIntento = prompt("intente de nuevo, ingrese numero del 1 al 5:");
+if (parseInt(nuevoIntento) === numeroAdivinar)
+
+{
+	adivinoCorrectamenete= true;
+}
+else
+{
+     adivinoCorrectamenete= false;
+ }
+}
+if (adivinoCorrectamenete)
+{
+	document.write("<p>adivinastes</p>");
+}
+else
+{
+	document.write("<p>no adivinastes</p>");
+}
 console.log("programa completo");
